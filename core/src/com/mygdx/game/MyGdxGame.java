e87114702ebde8522b94d5e5957fcf2a2719dfda
MOHAN <singhmohan333@gmail.com>
2017-09-07 13:44:53 +0530

    no message

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,6 +2,8 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -16,22 +18,17 @@ import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.math.Vector2;
 
 
-public class MyGdxGame extends ApplicationAdapter implements GestureDetector.GestureListener{
+public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
 	SpriteBatch batch;
-	private OrthographicCamera camera;
-	private Texture texture;
-	private Sprite sprite;
+	Sound gamesound;
+
 
 
 	@Override
 	public void create() {
 		batch = new SpriteBatch();
-		camera = new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
-		texture = new Texture(Gdx.files.internal("img.png"));
-		sprite = new Sprite(texture);
+		Gdx.input.setInputProcessor(this);
 
-		sprite.setPosition(-sprite.getWidth()/2,-sprite.getHeight()/2);
-		Gdx.input.setInputProcessor(new GestureDetector(this));
 
 
 	}
@@ -39,11 +36,11 @@ public class MyGdxGame extends ApplicationAdapter implements GestureDetector.Ges
 	public void render() {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.setProjectionMatrix(camera.combined);
+
 
 
 		batch.begin();
-		sprite.draw(batch);
+
 		batch.end();
 	}
 
@@ -51,56 +48,46 @@ public class MyGdxGame extends ApplicationAdapter implements GestureDetector.Ges
 	public void dispose() {
 
 		batch.dispose();
- 		texture.dispose();
 
 	}
 
 	@Override
-	public boolean pan(float x, float y, float deltaX, float deltaY) {
-		camera.translate(-deltaX,deltaY);
-		camera.update();
+	public boolean keyDown(int keycode) {
 		return false;
 	}
 
 	@Override
-	public boolean touchDown(float x, float y, int pointer, int button) {
-	return false;
-	}
-
-	@Override
-	public boolean tap(float x, float y, int count, int button) {
+	public boolean keyUp(int keycode) {
 		return false;
 	}
 
 	@Override
-	public boolean longPress(float x, float y) {
+	public boolean keyTyped(char character) {
 		return false;
 	}
 
 	@Override
-	public boolean fling(float velocityX, float velocityY, int button) {
-		return false;
-	}
-
-
-
-	@Override
-	public boolean panStop(float x, float y, int pointer, int button) {
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
 		return false;
 	}
 
 	@Override
-	public boolean zoom(float initialDistance, float distance) {
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
 		return false;
 	}
 
 	@Override
-	public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
 		return false;
 	}
 
 	@Override
-	public void pinchStop() {
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
 
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
